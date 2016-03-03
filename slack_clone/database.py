9c66dbdf7a5c25cce2c787c721ee554f7183e0ad
“student511” <abreu.wilbert@gmail.com>
2016-03-03 02:14:58 -0500

    heroku changes

--- slack_clone/database.py
+++ slack_clone/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+import os
 from slack_clone import app
 
-engine = create_engine(app.config["DATABASE_URL"])
+engine = create_engine(os.environ["DATABASE_URL"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
