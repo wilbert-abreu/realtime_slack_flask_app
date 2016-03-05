fd820a8e245a4dc853831bfdd551ba24487ade80
“student511” <abreu.wilbert@gmail.com>
2016-03-04 22:23:39 -0500

    pdb testing

--- slack_clone/__init__.py
+++ slack_clone/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_heroku import Heroku
+import pdb
 
 app = Flask(__name__)
 # config_path = os.environ.get("CONFIG_PATH",
 #                               "slack_clone.config.DevelopmentConfig")
 #  app.config.from_object(config_path)
 heroku = Heroku(app)
+
+pdb.set_trace()
 
 from . import views
 from . import login
